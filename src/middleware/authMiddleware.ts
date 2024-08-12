0a0f3910224ad09050a548d9f57f5f905a340d2c
Paulo Henrique <corlhog@gmail.com>
2024-08-12 00:14:05 -0300

    FEAT: Config para mesmo endpoint aceitarr requisições autenticadas e nao autenticadas

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { jwtConfig } from '../config/jwtConfig';
+import {Logger} from "sequelize/types/utils/logger";
 
 interface DecodedToken {
     userId: string;
@@ -50,6 +51,37 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
         next();
     } catch (error) {
         res.status(403).json({ msg: "Token inválido!" });
+    }
+};
+export const authenticateTokenGerarUrl = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log('VALIDAÇÃO IF',!token || token !== 'null' || token !== undefined)
+    if(!token && token !== null && token !== undefined){
+        console.log('TOKEN',token)
+        try {
+            const tokenParts = token.split('.');
+            if (tokenParts.length !== 3) {
+                return res.status(403).json({msg: "Token inválido!"});
+            }
+
+            jwt.verify(token, jwtConfig.secret, {algorithms: ['HS256']});
+
+            const decoded = jwt.decode(token) as DecodedToken;
+            if (decoded.exp < Date.now() / 1000) {
+                return res.status(403).json({msg: "Token expirado!"});
+            }
+
+            req.user = {
+                userId: decoded.userId,
+            };
+
+            next();
+        } catch (error) {
+            res.status(403).json({msg: "Token inválido!"});
+        }
+    }else {
+        next();
     }
 };
 
