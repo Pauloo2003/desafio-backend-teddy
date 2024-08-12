066bccfa356dcda36ac1d927a0645a57d538b280
Paulo Henrique <corlhog@gmail.com>
2024-08-12 00:52:01 -0300

    FEAT: AJUSTE FUNCAO LOGIN

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
-import { User } from '../models/User';
+import {UserRepository} from '../repositories/userRepository';
 import { jwtConfig } from '../config/jwtConfig';
 import bcrypt from 'bcrypt';
+import {logger} from "sequelize/types/utils/logger";
+const userRepository = new UserRepository();
 const generateTokens = (userId: number) => {
     const accessToken = jwt.sign({ userId}, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
     const refreshToken = jwt.sign({ userId }, jwtConfig.refreshTokenSecret, { expiresIn: jwtConfig.refreshTokenExpiresIn });
@@ -9,18 +11,16 @@ const generateTokens = (userId: number) => {
 };
 
 export const loginUser = async (email: string, password: string) => {
-    const user = await User.findOne({ where: { email } });
-
+    const user : any = await userRepository.findOne({ where: { email , deletedAt : null } });
     if (!user) {
         throw new Error('Credenciais Invalidas');
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user[0].password);
 
     if (!isMatch) {
         throw new Error('E-mail ou senha inválidos');
     }
-    return generateTokens(user.id);
+    return generateTokens(user[0].id);
 };
 
 export const refreshTokenService = (refreshToken: string) => {
